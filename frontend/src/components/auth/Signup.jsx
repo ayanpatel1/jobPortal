50e4378bf902c60157c20d64e404d86096250a50
Ayan patel <133395096+ayanpatel1@users.noreply.github.com>
2025-06-08 19:06:30 +0530

    Update Signup.jsx

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -93,7 +93,7 @@ const Signup = () => {
 
         try {
             dispatch(setLoading(true));
-            const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
+            const res = await axios.post("https://jobportal-1-qfkx.onrender.com/api/v1/user/register", formData, {
                 headers: {
                     "Content-Type": "multipart/form-data"
                 },
